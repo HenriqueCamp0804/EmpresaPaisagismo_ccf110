69a436b396135eb3be77223a6438da44b3d5dd60
HenriqueCampos <henriquecampos0804@gmail.com>
2021-12-22 19:03:48 -0300

    Menu de opcoes(planta mais cara,mais barata,cadastro,media,valor total). Adicao da verificacao de estoque,correçao de erros. Introducao do conceito de vetores para somar a quantidade de clientes na semana.

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 int main()
 {
     int opcao,quantidadePlantas,valorMenor=0,valorMaior=0,estoqueAtual=0,quantidadeEstoque=50,estoqueIdeal=30;
-    int estoqueMinimo;
+    int estoqueMinimo,vetNumClientes[7],i,somaClientes;
     char plantaCara[30],plantaBarata[30];
     float media,soma=0,valorPlanta;
     char nomePlanta[50];
@@ -16,7 +16,8 @@ int main()
         printf("4 - Planta mais barata\n");
         printf("5 - Valor total das plantas\n");
         printf("6 - Verificar estoque\n");
-        printf("7 - Sair\n");
+        printf("7 - Quantidade de clientes na semana:\n");
+        printf("8 - Sair\n");
         printf("Opcao: ");
         scanf("%d",&opcao);
         if (opcao == 1) {
@@ -38,6 +39,8 @@ int main()
                 valorMenor = valorPlanta;
                 strcpy(plantaBarata,nomePlanta);
             }
+
+
         } else if (opcao == 2) {
             printf("Media dos valores das plantas = %.2f\n",media);
 
@@ -62,8 +65,19 @@ int main()
                 printf("O estoque desta planta esta acima do ideal!\n");
             }
         }else if (opcao == 7){
+            for(i=0;i<7;i++){
+                printf("Entre com a quantidade de clientes do dia %d: ",i+1);
+                scanf("%d",&vetNumClientes[i]);
+            }
+            for(i=0;i<7;i++){
+                somaClientes = somaClientes + vetNumClientes[i];
+            }
+            printf("A quantidade de clientes da semana foi de: %d\n",somaClientes);
+
+        } else if(opcao == 8 ){
             break;
-        } else{
+
+        }else{
             printf("Opcao invalida!");
         }
 
